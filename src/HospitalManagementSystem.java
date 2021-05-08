c0e72add9c3ae38ef4d3cfd13f1d0ced6969d3ec
vidushi1012 <iit2019027@iiita.ac.in>
2021-05-08 12:04:37 +0530

    project files

---
+++ src/HospitalManagementSystem.java
@@ -0,0 +1,13 @@
+
+public class HospitalManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Index frame = new Index();
+	frame.setVisible(true);
+    }
+    
+}
